b4a50694995679bebe423e9dd64ed7a4b2cb76af
= <rstancu559@boscdelacoma.cat>
2023-02-14 19:34:58 +0100

    s

---
+++ src/app/models/artista.ts
@@ -0,0 +1,10 @@
+export interface ArtistaInterface{
+    id: number;
+    title: string;
+    artist_title: string;
+    date_display: string;
+    image_id: number;
+    thumbnail: string;
+    image_url: string;
+    artist_display: string;
+}
